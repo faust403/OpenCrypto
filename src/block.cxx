cb5d8ad726071a4bdbf98483c09be98e5e3e0639
Werner von Faust <vl.averkov@mail.ru>
2022-08-24 10:08:40 -0400

    .

--- src/block.cxx
+++ src/block.cxx
@@ -1,3 +1,19 @@
 #include <libcryptocxx/block.h>
 
 int add(int, int);
+
+template <std::uint64_t>
+class Block
+{
+      public:
+            constexpr explicit Block(void) noexcept = default;
+            ~Block(void) noexcept                   = default;
+};
+
+template <std::uint64_t>
+class VBlock : public Block
+{
+      public:
+            constexpr explicit Block(void) noexcept = default;
+            ~Block(void) noexcept                   = default;
+};
