efedab6c5ad5cf5192189b2b3a027b18a5061b5d
Werner von Faust <vl.averkov@mail.ru>
2022-08-28 08:58:55 -0400

    Operators overloading in Byte

--- include/opencrypto/block.h
+++ include/opencrypto/block.h
@@ -24,13 +24,21 @@ class Byte
             }
             virtual ~Byte(void) noexcept = default;
 
-            constexpr Byte &           hex(void) noexcept;
-            constexpr Byte &           bin(void) noexcept;
-            constexpr Byte &           transform(const short) noexcept;
-            constexpr Byte &           format(void) noexcept;
-            constexpr unsigned char    integral(void) noexcept;
-            constexpr std::string_view str(void) noexcept;
-            constexpr unsigned char &  get(void) noexcept;
+            constexpr std::string_view hex(void) noexcept;
+            constexpr std::string_view bin(void) noexcept;
+            constexpr std::string_view dec(void) noexcept;
+
+            template <typename Type>
+            constexpr Byte & transform(const Type NS) noexcept
+            {
+                  static_assert(std::is_integral_v<Type>,
+                                "Method Byte::transform(const Type) have got non-integral Type as argument. Integral Required");
+                  return *this;
+            }
+
+            constexpr unsigned char ascii(void) noexcept;
+
+            constexpr unsigned char get(void) noexcept;
             template <typename Type>
             constexpr void set(const Type Data) noexcept
             {
@@ -42,42 +50,60 @@ class Byte
             }
 
             // Binary operators
-            constexpr Byte & operator= (const Byte & Other) noexcept;
-            constexpr Byte & operator== (const Byte & Other) noexcept;
-            constexpr Byte & operator!= (const Byte & Other) noexcept;
-            constexpr Byte & operator|| (const Byte & Other) noexcept;
-            constexpr Byte & operator|= (const Byte & Other) noexcept;
-            constexpr Byte & operator&& (const Byte & Other) noexcept;
-            constexpr Byte & operator&= (const Byte & Other) noexcept;
-            constexpr Byte & operator^ (const Byte & Other) noexcept;
-            constexpr Byte & operator^= (const Byte & Other) noexcept;
+            constexpr bool operator== (Byte & Other) noexcept { return this->__Byte == *Other; }
+            constexpr bool operator!= (Byte & Other) noexcept { return this->__Byte != *Other; }
+            Byte           operator|| (Byte & Other) noexcept { return Byte{static_cast<unsigned char>(this->__Byte || *Other)}; }
+            Byte           operator&& (Byte & Other) noexcept { return Byte{static_cast<unsigned char>(this->__Byte && *Other)}; }
+            Byte           operator^ (Byte & Other) noexcept { return Byte{static_cast<unsigned char>(this->__Byte ^ *Other)}; }
+            constexpr Byte & operator= (Byte & Other) noexcept
+            {
+                  this->__Byte = *Other;
+                  return *this;
+            }
+            constexpr Byte & operator&= (Byte & Other) noexcept
+            {
+                  this->__Byte &= *Other;
+                  return *this;
+            }
+            constexpr Byte & operator|= (Byte & Other) noexcept
+            {
+                  this->__Byte |= *Other;
+                  return *this;
+            }
+            constexpr Byte & operator^= (Byte & Other) noexcept
+            {
+                  this->__Byte ^= *Other;
+                  return *this;
+            }
             template <typename Type>
-            constexpr Byte & operator>> (const Type Shift) noexcept
+            constexpr Byte operator>> (const Type Shift) noexcept
             {
                   static_assert(std::is_integral_v<Type>, "operator >> have got non-integral operand. Integral required");
-                  return *this;
+                  return Byte{this->__Byte >> Shift};
             }
             template <typename Type>
             constexpr Byte & operator>>= (const Type Shift) noexcept
             {
                   static_assert(std::is_integral_v<Type>, "operator >>= have got non-integral operand. Integral required");
+                  this->__Byte >>= Shift;
                   return *this;
             }
             template <typename Type>
-            constexpr Byte & operator<< (const Type Shift) noexcept
+            constexpr Byte operator<< (const Type Shift) noexcept
             {
                   static_assert(std::is_integral_v<Type>, "operator << have got non-integral operand. Integral required");
-                  return *this;
+                  return Byte{this->__Byte << Shift};
             }
             template <typename Type>
             constexpr Byte & operator<<= (const Type Shift) noexcept
             {
                   static_assert(std::is_integral_v<Type>, "operator <<= have got non-integral operand. Integral required");
+                  this->__Byte <<= Shift;
                   return *this;
             }
             // Unary operators
-            constexpr Byte & operator!(void) noexcept;
-            constexpr Byte & operator* (const Byte & Other) noexcept;
+            constexpr unsigned char & operator* (void) noexcept { return this->__Byte; }
+            Byte                      operator!(void) noexcept { return Byte{static_cast<unsigned char>(!this->__Byte)}; }
 };
 
 #endif
--- src/block.cxx
+++ src/block.cxx
@@ -1,3 +1,1 @@
 #include <opencrypto/block.h>
-
-int add(int a, int b) { return a + b; }
